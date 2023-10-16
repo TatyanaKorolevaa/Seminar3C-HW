b783193b125efc6010396c3b2d8a54cb2774c21a
TatyanaKorolevaa <t.korolevaaa@mail.ru>
2023-10-16 12:11:45 +0300

    Task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+char numbers(string chislo, int num)
+{
+    if (chislo.Length >= num)
+    {
+        return chislo[num - 1];
+    }
+    else
+    {
+        Console.Write($"{num}-й элемент отсутствует");
+    }
+return '!';
+}
+Console.WriteLine("Введите число трехзначное");
+Console.WriteLine(numbers(Console.ReadLine(),3));
